6472dd267a0fc793c3beb2c533da7db024d2e4cb
Dani Riera <daniel.riera@qatium.com>
2021-12-15 20:44:32 +0100

    Add gradient to spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -9,6 +9,10 @@ type SpinnerProps = {
 const Spinner = ({ elapsed, total, color }: SpinnerProps): JSX.Element => {
   return (
     <svg viewBox="0 0 100 100" className={classes.spinner}>
+      <linearGradient id="dark" x1="1" y1="0.5" x2="0" y2="0.5">
+        <stop offset="0%" stopColor="var(--black-color)" />
+        <stop offset="30%" stopColor="transparent" />
+      </linearGradient>
       <circle
         cx="50"
         cy="50"
@@ -18,6 +22,20 @@ const Spinner = ({ elapsed, total, color }: SpinnerProps): JSX.Element => {
         strokeWidth={2}
         strokeDasharray="310 310"
         strokeDashoffset={310 + (310 * elapsed) / total}
+      />
+      <circle
+        cx="50"
+        cy="50"
+        r="49"
+        fill="none"
+        stroke="url(#dark)"
+        strokeWidth={2.1}
+        strokeDasharray="310 310"
+        strokeDashoffset={(220 * elapsed) / total}
+        style={{
+          transform: `rotate(-${(360 * elapsed) / total}deg)`,
+          transformOrigin: "50% 50%",
+        }}
       />
     </svg>
   );
